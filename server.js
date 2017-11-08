845663efa9e71dbcdf976cb33108eeee91a11b51
Tim Horist <TimelikeClosure@users.noreply.github.com>
2017-11-07 19:02:52 -0800

    added basic file serving

--- server.js
+++ server.js
@@ -1,15 +1,56 @@
 "use strict";
 
 const http = require('http');
+const URL = require('url');
+const path = require('path');
+const fs = require('fs');
 
-const HOST = '127.0.0.1';
-const PORT = 3000;
+const mimeTypes = {
+    html: 'text/html',
+    jpeg: 'image/jpeg',
+    jpg: 'image/jpg',
+    png: 'image/png',
+    js: 'text/javascript',
+    css: 'text/css'
+}
 
 http.createServer(function(req, res){
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-    res.end('Hello my honey, hello my darlin\', hello my ragtime gal');
-}).listen(PORT, HOST, function(){
-    console.log(`${new Date()}: The server is now running on ${HOST}:${PORT}`);
-});
+    var uri = URL.parse(req.url).pathname;
+    var fileName = path.join(process.cwd(), unescape(uri));
+    console.log(`Loading ${uri}`);
+    var stats;
+
+    try{
+        stats = fs.lstatSync(fileName);
+    } catch(e){
+        res.writeHead(404, {
+            'Content-type': 'text/plain'
+        });
+        res.write('404 Not Found\n');
+        res.end();
+        return;
+    }
+
+    if(stats.isFile()){
+        var mimeType = mimeTypes[path.extname(fileName).split('.').reverse()[0]]
+        res.writeHead(200, {
+            "Content-type": mimeType
+        });
+        var fileStream = fs.createReadStream(fileName);
+        fileStream.pipe(res);
+    } else if (stats.isDirectory()){
+        res.writeHead(302, {
+            'Location': uri + 'index.html'
+        })
+        res.end();
+    } else {
+        res.writeHead(500, {
+            'Content-type': 'text/plain'
+        });
+        res.write('500 Internal Error\n');
+        res.end();
+    }
+
+}).listen(3000, function(){
+    console.log(`${new Date()} Listening on port 3000`);
+})
